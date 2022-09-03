09813353487e68b73ce77bcb29a7c483294ea9c8
Nandi Wong <nandi0315@gmail.com>
2022-09-03 11:42:46 -0400

    ✨ feat: gatsby 4

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import Header from './Header';
 
 interface Props {
@@ -14,11 +13,9 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
-  const { t } = useTranslation();
-
   return (
     <div className="items-center flex flex-col h-full">
-      <Header siteTitle={t('title')} />
+      <Header />
       <div className="p-8 w-full lg:w-1/2 flex flex-col">
         <main className="h-full">{children}</main>
         <footer>
